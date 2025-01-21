8c7dc0004de03f88772d030baf9cb55e62d21ceb
Mohammed-ZakiUllah Bhingraj <zakibhingraj@yahoo.com>
2025-01-21 14:28:24 +0000

    created books controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'books/index'
+  get 'books/show'
+  get 'books/new'
+  get 'books/create'
   root to: "pages#home"
   resources :books, only: [:index, :show, :new, :create] do
     resources :loans, only: [:new, :create]
