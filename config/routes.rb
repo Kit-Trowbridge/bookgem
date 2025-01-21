c543ae86b5178393fc286e5a58fe8311e9b70004
Rita Starzyk <rita_s@mail.com>
2025-01-21 14:04:53 +0000

    loans controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,3 @@
-# Rails.application.routes.draw do
-#   root to: "pages#home"
-#   resources :books, only: [:index, :show, :new, :create] do
-#     resources :loans, only: [:new, :create]
-#   end
-#   resources :loans, only: [:index]
-# end
-
-
 Rails.application.routes.draw do
   root to: "pages#home"
   resources :books, only: [:index, :show, :new, :create] do
