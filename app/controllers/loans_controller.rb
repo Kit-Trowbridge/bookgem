e127768addaa8dfecf1cd9c220dd258f7d91977c
Kit Trowbridge <kittrow.dev@gmail.com>
2025-01-22 13:38:09 +0000

    created seed file, added bootstrap to application.scss, started work on new loan form

--- app/controllers/loans_controller.rb
+++ app/controllers/loans_controller.rb
@@ -2,5 +2,28 @@ class LoansController < ApplicationController
   def index
     @loans = Loan.all
   end
-end
 
+  def new
+    @loan = Loan.new
+  end
+
+  def create
+    @users = User.all
+    @book = Book.find(params[:book_id])
+    @loan = Loan.new(loan_params)
+    @loan.book = @car
+    if @loan.save
+      # need this even though it doesn't bring to new page
+      redirect_to book_loans_path(@book)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  # how do I make it so that the user is automatic (signed in - shouldn't be something to select)
+  def loan_params
+    params.require(:loan).permit(:start_date, :end_date, :user_id)
+  end
+end
