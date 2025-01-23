d71af68017ad33cfeb5dd2685867c01a59b88a63
Kit Trowbridge <kittrow.dev@gmail.com>
2025-01-23 17:13:52 +0000

    set loan user to current user but still need to DRY code

--- app/controllers/loans_controller.rb
+++ app/controllers/loans_controller.rb
@@ -6,12 +6,15 @@ class LoansController < ApplicationController
   def new
     @loan = Loan.new
     @book = Book.find(params[:book_id])
+    @user = current_user
   end
 
   def create
     @book = Book.find(params[:book_id])
     @loan = Loan.new(loan_params)
+    @user = current_user
     @loan.book = @book
+    @loan.user = @user
     if @loan.save
       redirect_to loans_path
     else
@@ -23,6 +26,6 @@ class LoansController < ApplicationController
 
   # how do I make it so that the user is automatic (signed in - shouldn't be something to select)
   def loan_params
-    params.require(:loan).permit(:start_date, :end_date, :user_id)
+    params.require(:loan).permit(:start_date, :end_date)
   end
 end
